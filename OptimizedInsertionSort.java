d80699a2a420b6b6a541718805fc1462502896a0
chathu0901 <ncsuraweera2006@gmail.com>
2026-01-20 13:16:36 +0530

    Update OptimizedInsertionSort.java

--- OptimizedInsertionSort.java
+++ OptimizedInsertionSort.java
@@ -17,7 +17,6 @@ public class OptimizedInsertionSort {
             int key = arr[i];
             int j = i - 1;
 
-            // Shift only when needed (optimized)
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j--;
@@ -35,3 +34,4 @@ public class OptimizedInsertionSort {
     }
     
 }
+
